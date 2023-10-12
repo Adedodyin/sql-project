b13ae74c54d13475738885b430402191b970b56f
Adedoyin Ademola <142696103+Adedodyin@users.noreply.github.com>
2023-10-12 22:25:22 +0100

    Oldest business_SQL.sql

--- Oldest business_SQL.sql
+++ Oldest business_SQL.sql
@@ -1,6 +1,32 @@
 /****** Script for SelectTopNRows command from SSMS  ******/
 SELECT TOP (1000) [business]
       ,[year_founded]
+      ,[category_code]
+      ,[country_code]
+  FROM [Case Study].[dbo].[businesses$]
+
+/* VIEWING DATA FROM ALL 3 TABLES */
+SELECT *
+FROM [Case Study].dbo.businesses$
+
+SELECT * 
+FROM [Case Study].dbo.categories$
+
+SELECT *
+FROM [Case Study].dbo.countries$
+
+	
+/* JOINING ALL TABLES */
+SELECT business, year_founded, category,business_1.category_code,business_1.country_code, country, continent
+FROM [Case Study].dbo.businesses$ business_1
+INNER JOIN [Case Study].dbo.categories$ category_1
+ON business_1.category_code = category_1.category_code
+INNER JOIN [Case Study].dbo.countries$ country_1
+ON business_1.country_code = country_1.country_code
+
+/* VIEWING THE JOINED TABLES */
+SELECT TOP (1000) [business]
+      ,[year_founded]
       ,[category]
       ,[category_code]
       ,[country_code]
@@ -9,14 +35,12 @@ SELECT TOP (1000) [business]
   FROM [Case Study].[dbo].[oldest_business_joined]
 
 
-/* Top 5 oldest business still existing */
+/* FINDING THE TOP 5 OLDEST BUSINESSES STILL EXISTING */
 SELECT TOP 5 business, year_founded, category, country
 FROM [Case Study].[dbo].[oldest_business_joined]
 ORDER BY year_founded ASC
 
-/* JAPAN - 578, AUSTRIA - 803, GERMANY - 862, FRANCE - 864, UNITED KINGDOM - 886. Construction is the oldest category of busines still existing. Oldest Categories still existing is Cafes Restaurant and Bars - 803 in Austria, Distilers Vintners & Breweries - 862 in Germany, Manufacturing - 886 UK and France*/
-
-/* Categories with most businesses */
+/* FINDING THE CATEGORIES WITH MOST BUSINESSES (2 SETS OF CODE)*/
 SELECT DISTINCT category, business,
 COUNT(business) OVER (PARTITION BY category) AS Count_of_Category
 FROM dbo.oldest_business_joined
@@ -26,23 +50,18 @@ FROM dbo.oldest_business_joined
 GROUP BY category 
 ORDER BY count_of_business DESC
 
-/* Banking & Finance - 37, Distillers Vinters and Breweries - 22, Aviation and Transport - 19, Postal Service - 16, Manufacturing & Production = 15 */
-
+/* FINDING THE CONTINENTS WITH THE MOST OLD BUSINESSES STILL EXISTING */
 SELECT COUNT (business) AS count_of_business, continent 
 FROM dbo.oldest_business_joined
 GROUP BY continent
 
-/* Africa is the continent with most oldest business still existing with 50 businesses and Oceania is the continent with the lowest businesses still existing at 3 businesses. */
-
+/* FINDING THE CONTINENTS WITH THE MOST CATEGORIES STILL EXISTING */
 SELECT COUNT (DISTINCT category) AS count_of_category, continent
 FROM dbo.oldest_business_joined
 GROUP BY continent
 ORDER BY count_of_category DESC
 
-/* Asia has the most categories at 17 while Oceania has the lowest at 2 */
-
-/* Oldest Business and Category Per Continent */
-
+/* FINDING THE OLDEST BUSINESS AND CATEGORY PER CONTINENT */
 WITH RankedBusinesses AS (
   SELECT
     business,
@@ -65,28 +84,7 @@ WHERE
   RowNum = 1
   ORDER BY year_founded
 
-
-  WITH RankedCategories AS (
-  SELECT
-    continent,
-    year_Founded,
-	category,
-    ROW_NUMBER() OVER(PARTITION BY continent ORDER BY year_founded ASC) AS RowNum
-  FROM
-    [Case Study].dbo.oldest_business_joined
-)
-
-SELECT
-  Distinct continent,
-  year_founded,
-  category
-FROM
-  RankedCategories
-WHERE
-  RowNum = 1
-   ORDER BY year_founded
-
-
+/* FINDING THE OLDEST BUSINESS PER CATEGORY */
 WITH RankedBusinesses AS (
   SELECT
     business,
