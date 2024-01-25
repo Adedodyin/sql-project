8af7e9d129db4eee00a28b1ac511b81f78e8549d
Adedoyin Ademola <142696103+Adedodyin@users.noreply.github.com>
2024-01-25 23:44:47 +0100

    Train Details.sql

--- Train Details.sql
+++ Train Details.sql
@@ -1,4 +1,4 @@
-/****** Script for SelectTopNRows command from SSMS  ******/
+/* Selecting the TOP 1000 to get an overview of the dataset*/
 SELECT TOP (1000) [Train No]
       ,[Station Code]
       ,[Station Name]
@@ -12,6 +12,7 @@ SELECT TOP (1000) [Train No]
       ,[Destination Station Name]
   FROM [SQL Tutorial].[dbo].[Train_details$];
 
+/* To calculate the duration*/
  
 SELECT *, CAST(dbo.Train_details$.[Arrival time] AS time) AS arrive, 
 CAST([SQL Tutorial].dbo.Train_details$.[Departure Time] AS time) AS depart, 
@@ -19,3 +20,17 @@ DATEDIFF(second, CAST(dbo.Train_details$.[Arrival time] AS time),
 CAST([SQL Tutorial].dbo.Train_details$.[Departure Time] AS time)) AS duration
 FROM dbo.Train_details$;
 
+/* To find the total number of trains*/
+
+SELECT COUNT(dbo.Train_details$.[Train No]) AS Total_trains
+FROM dbo.Train_details$;
+
+/* To find the unique train numbers*/
+
+SELECT COUNT(DISTINCT(dbo.Train_details$.[Train No]))
+FROM dbo.Train_details$;
+
+/* to find the unique source and destination station*/
+SELECT COUNT(DISTINCT(dbo.Train_details$.[Source Station Name])) AS source_station, 
+COUNT(DISTINCT(dbo.Train_details$.[Destination Station Name])) AS destination_station
+FROM dbo.Train_details$;
