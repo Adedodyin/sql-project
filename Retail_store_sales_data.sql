df10f88f59181c0fcf69090bfef3a75170a6697f
Adedoyin Ademola <142696103+Adedodyin@users.noreply.github.com>
2024-03-11 21:26:41 +0100

    Retail_store_sales_data.sql

--- Retail_store_sales_data.sql
+++ Retail_store_sales_data.sql
@@ -10,3 +10,15 @@ SELECT TOP (10) [transaction_id]
       ,[discount_amount]
       ,[store_location]
   FROM [Case Study].[dbo].[retail_store]
+
+/* To find the total revenue generated from all transactions? */
+  SELECT SUM(unit_price * quantity) AS revenue 
+  FROM retail_store;
+
+/* To find the different payment methods used by customers */
+SELECT DISTINCT payment_method AS different_payment_method
+FROM retail_store;
+
+SELECT COUNT(customer_id), payment_method 
+FROM retail_store 
+GROUP BY payment_method;
