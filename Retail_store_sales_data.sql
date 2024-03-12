baccfb12623b8974bb1e06958a48d1c25e229410
Adedoyin Ademola <142696103+Adedodyin@users.noreply.github.com>
2024-03-12 21:33:47 +0100

    Retail_store_sales_data.sql

--- Retail_store_sales_data.sql
+++ Retail_store_sales_data.sql
@@ -22,3 +22,17 @@ FROM retail_store;
 SELECT COUNT(customer_id), payment_method 
 FROM retail_store 
 GROUP BY payment_method;
+
+/* To find the average amount spent per transaction */
+SELECT AVG(total_amount) AS average_amount_per_transaction
+FROM retail_store;
+
+/* To find the distribution of dates over time */
+SELECT payment_date, AVG(total_amount) AS average_amount
+FROM retail_store
+GROUP BY payment_date
+ORDER BY payment_date ASC;
+
+SELECT MIN(payment_date) AS earliest_date, 
+MAX(payment_date) AS latest_date
+FROM retail_store;
